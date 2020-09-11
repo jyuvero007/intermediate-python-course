647c2034ae62b673bca387d513fa146f45398f5f
jyuvero007 <70600786+jyuvero007@users.noreply.github.com>
2020-09-11 12:57:56 -0400

    Add more inputs (like player or team name). Store each player's roll totals in separate arrays.

--- dice_roller.py
+++ dice_roller.py
@@ -1,21 +1,29 @@
 import random
 
 def main():
+  playerCount = int(input('How players will be playing? '))
+  for i in range(0, playerCount):
+    player = input('\nWhat is your name? ')
+    dice_rolls = int(input('How many dice would you like to roll? '))
+    dice_size = int(input('How many sides are the dice? '))
+    print(f'Here are your results {player}:')
+    dice_sum = 0
+    ary = [] # make array of 0 length
+    for i in range(0, dice_rolls):
+      roll = random.randint(1, dice_size)
+      dice_sum += roll
+      ary.append(roll) # add the current players rolls into it
+      if roll == 1:
+        print(f'You rolled a {roll}! Critical Fail')
+      elif roll == dice_size:
+        print(f'You rolled a {roll}! Critical Success!')
+      else:
+        print(f'You rolled a {roll}')
 
-  dice_rolls = int(input('How many dice would you like to roll? '))
-  dice_size = int(input('How many sides are the dice? '))
-  dice_sum = 0
-  for i in range(0, dice_rolls):
-    roll = random.randint(1, dice_size)
-    dice_sum += roll
-    if roll == 1:
-      print(f'You rolled a {roll}! Critical Fail')
-    elif roll == dice_size:
-      print(f'You rolled a {roll}! Critical Success!')
-    else:
-      print(f'You rolled a {roll}')
 
-  print(f'You have rolled a total of {dice_sum}')
+    print(f'All the numbers {player} rolled: {ary}')
+    print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
+
   main()
